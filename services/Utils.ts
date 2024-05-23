fc2593332117c9bad881033a7b4701fa0295d3c5
Carsten Gräf <evandor@gmail.com>
2024-05-23 10:59:48 +0200

    submodules refactoring

--- services/Utils.ts
+++ services/Utils.ts
@@ -62,23 +62,6 @@ export function useUtils() {
       });
     }
   }
-  const isCurrentTab = (tab: Tab) => {
-    if (!inBexMode() || !tab.url) {
-      return false
-    }
-    const windowId = useWindowsStore().currentChromeWindow?.id || 0
-    const currentChromeTab = useTabsStore2().getCurrentChromeTab(windowId) || useTabsStore2().currentChromeTab
-    //console.log("checking current tab", currentChromeTab.url, tab.url, currentChromeTab.url === tab.url)
-    if (currentChromeTab?.url === tab.url) {
-      tab.chromeTabId = currentChromeTab.id
-      return true
-    }
-    //console.log("checking", currentChromeTab.url, "/" + btoa(tab.url || ''), currentChromeTab.url?.indexOf("/" + btoa(tab.url || '')) )
-    if (currentChromeTab?.url && currentChromeTab.url?.indexOf("/" + btoa(tab.url || '')) >= 0) {
-      return true
-    }
-    return false
-  }
 
   function urlToHost(url: string): string {
     try {
@@ -119,7 +102,6 @@ export function useUtils() {
     sanitize,
     sanitizeAsText,
     sendMsg,
-    isCurrentTab,
     calcHostList,
     favIconFromUrl
   }
