700ade9a7569e6aba0f8b22d9e12af0372df4cf1
Carsten Gräf <evandor@gmail.com>
2024-06-21 08:44:46 +0200

    work in progress

--- domain/ExecutionResult.ts
+++ domain/ExecutionResult.ts
@@ -1,11 +1,14 @@
 import Command from "src/core/domain/Command";
 
+/**
+ * the nextCommands parameter can be constructed like this:
+ * new Map([["Undo", new UndoCommand(tabId)]]
+ */
 export class ExecutionResult<T> {
   constructor(
     public result: T,
     public message: string,
     public nextCommands: Map<string, Command<any>> = new Map()) {
-
   }
 
 
