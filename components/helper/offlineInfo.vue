e29db0a65e48fe4db55b27b7ee7e17b905ba67d1
Carsten Gräf <evandor@gmail.com>
2024-07-20 09:43:12 +0200

    wip

---
+++ components/helper/offlineInfo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div v-if="!useUiStore().networkOnline"
+       class="q-ma-md q-pa-md justify text-body1" style="border:1px dotted red">
+    Sorry, you seem to be offline... try to reload in some minutes
+  </div>
+
+</template>
+
+<script lang="ts" setup>
+
+import {useUiStore} from "src/ui/stores/uiStore";
+</script>
