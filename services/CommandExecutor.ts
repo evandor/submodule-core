6648b1f90ad8a3fef805d01bd9c0105c616c94c1
Carsten Gräf <evandor@gmail.com>
2024-10-16 13:41:49 +0200

    github backup and logs

--- services/CommandExecutor.ts
+++ services/CommandExecutor.ts
@@ -1,34 +1,41 @@
 import Command from "src/core/domain/Command";
 import {NotificationType, useNotificationHandler} from "src/core/services/ErrorHandler";
 import {ExecutionFailureResult, ExecutionResult} from "src/core/domain/ExecutionResult";
+import {useUiStore} from "src/ui/stores/uiStore";
 
 const {handleSuccess, handleError} = useNotificationHandler()
 
 export function useCommandExecutor() {
 
-    const executeFromUi = (command: Command<any>, type: NotificationType = NotificationType.TOAST): Promise<ExecutionResult<any>> => {
-        console.log(" * executing command", command)
-        return command.execute()
-            .then((res) => handleSuccess(res, type))
-            .catch(err => {
-                console.log("error in command", command)
-                handleError(err, type)
-                return new ExecutionFailureResult(null, err)
-            })
-    }
+  const executeFromUi = async (command: Command<any>, type: NotificationType = NotificationType.TOAST): Promise<ExecutionResult<any>> => {
+    console.log(" * executing command", command)
+    useUiStore().commandExecuting = true
+    return command.execute()
+      .then((res: ExecutionResult<any>) => {
+        useUiStore().commandExecuting = false
+        return res
+      })
+      .then((res) => handleSuccess(res, type))
+      .catch(err => {
+        console.log("error in command", command)
+        useUiStore().commandExecuting = false
+        handleError(err, type)
+        return new ExecutionFailureResult(null, err)
+      })
+  }
 
-    const execute = (command: Command<any>): Promise<ExecutionResult<any>> => {
-        console.log(" * executing command", command)
-        return command.execute()
-            .catch(err => {
-                console.log("error in command", command)
-                handleError(err)
-                return new ExecutionFailureResult(null, err)
-            })
-    }
+  const execute = async (command: Command<any>): Promise<ExecutionResult<any>> => {
+    console.log(" * executing command", command)
+    return command.execute()
+      .catch(err => {
+        console.log("error in command", command)
+        handleError(err)
+        return new ExecutionFailureResult(null, err)
+      })
+  }
 
-    return {
-        executeFromUi,
-        execute
-    }
+  return {
+    executeFromUi,
+    execute
+  }
 }
