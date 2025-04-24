49d30f92989b6aab5949bf822f0911bdb9316e29
Carsten Gräf <evandor@gmail.com>
2025-04-24 11:43:15 +0200

    safepoint

--- services/Logger.ts
+++ services/Logger.ts
@@ -9,6 +9,51 @@ const version = import.meta.env.PACKAGE_VERSION
 const { setupErrorHandling } = useErrorHandlingConfig()
 var scope = setupErrorHandling()
 
+const postLogsToLoki = async (message: string, labels: object) => {
+  const lokiURL = 'https://logs-prod-012.grafana.net/loki/api/v1/push' // Replace with your Loki URL
+  const headers = {
+    'Content-Type': 'application/json',
+    // Add any other custom headers you need
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    Authorization: `Basic ${btoa(`${process.env.GRAFANA_LOKI_USER as string}:${process.env.GRAFANA_LOKI_TOKEN as string}`)}`,
+  }
+
+  const logData = {
+    streams: [
+      {
+        stream: Object.assign({ level: 'info' }, labels),
+        values: [[`${Date.now().toString()}000000`, JSON.stringify(message)]],
+      },
+    ],
+    // streams: [
+    //   {
+    //     stream: {
+    //       lablename: 'test lable', // Replace with your labels
+    //     },
+    //     values: [
+    //       [new Date().toISOString(), 'post message from culture'],
+    //       // Add more log entries as needed
+    //     ],
+    //   },
+    // ],
+  }
+
+  try {
+    // const response = await axios.post(lokiURL, logData,{ headers }).catch((e)=>{
+    //   console.log(e);
+    // });
+    fetch(lokiURL, { method: 'POST', headers, body: JSON.stringify(logData) }).then((response) => {
+      response.json().then((data) => {
+        console.log('data', data)
+      })
+    })
+  } catch (error) {
+    console.error('Error posting logs to Loki:', error)
+  }
+}
+
 async function log(msg: string, level: number) {
   // console.log('sending message to sentry...', scope)
   // scope.captureMessage(msg)
@@ -25,10 +70,16 @@ async function log(msg: string, level: number) {
       { _mode: process.env.MODE || 'unknown', _version: version, service_name: EXTENSION_NAME },
     )
   } else {
-    await logger.info(
-      { message: msg },
-      { _mode: process.env.MODE || 'unknown', _version: version, service_name: EXTENSION_NAME },
-    )
+    // await logger.info(
+    //   { message: msg },
+    //   { _mode: process.env.MODE || 'unknown', _version: version, service_name: EXTENSION_NAME },
+    // )
+    postLogsToLoki(msg, {
+      _mode: process.env.MODE || 'unknown',
+      _version: version,
+      _logger: 'postlogstoloki',
+      service_name: EXTENSION_NAME,
+    })
   }
 
   // const gelfMessage = {
@@ -56,7 +107,9 @@ async function log(msg: string, level: number) {
 
 export function useLogger() {
   const info = (msg: string) => {
-    log(msg, 5)
+    if (process.env.MODE === 'bex') {
+      log(msg, 5) // do not log for pwa, there's faro-collector for that
+    }
   }
 
   const error = (msg: string) => {
